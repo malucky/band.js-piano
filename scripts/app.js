fe38758947182ad6c68a435c546f34c929facc3b
Daniel Almaguer <danielalmaguer@gmail.com>
2014-04-13 16:57:46 -0700

    Started logic for converting to JSON

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 (function() {
 
+  var music = new BandJS();
   var currentNotes = {};
+  var songNotes = [];
 
   var addNote = function(el) {
     var key = el.data('note');
@@ -14,11 +16,39 @@
     el.removeClass('selected');
   };
 
-  var displayNotes = function() {
+  var addNotesToSong = function() {
     for(var key in currentNotes) {
       currentNotes[key].removeClass('selected');
     }
-    console.log(Object.keys(currentNotes));
+    var notes = Object.keys(currentNotes).join(', ');
+    songNotes.push([notes, 'quarter']);
+  };
+
+  var toJSON = function() {
+    var result = {};
+    result.timeSignature = [4, 4];
+    result.tempo = 100;
+    result.instruments = {
+      rightHand: {
+        name: 'square',
+        pack: 'oscillators'
+      }
+    };
+    result.notes = {
+      rightHand: []
+    };
+
+    songNotes.forEach(function(note) {
+      result.notes.rightHand.push({
+        type: 'note',
+        pitch: note[0],
+        rhythm: note[1]
+      });
+    });
+
+    music.load(result);
+    music.end();
+    music.play();
   };
 
   $(document).ready(function() {
@@ -31,7 +61,9 @@
       }
     });
     // Display notes
-    $('button').on('click', displayNotes);
+    $('.add').on('click', addNotesToSong);
+    $('.play').on('click', toJSON);
+    toJSON();
   });
 
 })();
