69dd5e90a99e9ab128b9c184e6744584495796f4
Ray Ma <rhm262@gmail.com>
2014-04-13 17:04:12 -0700

    Displaying current notes and implemented notes duration

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,66 @@
-(function() {
+(function () {
 
   var currentNotes = {};
+  var currentNotesDuration = "";
 
-  var addNote = function(el) {
+  var keyMap = {
+    '52': 'quarter',
+    '56': 'eighth',
+    '49': 'whole'
+  };
+
+  var $notesDurationDisplay = $('.note-length');
+  var $currentNotesDisplay = $('.current-notes');
+
+
+  var addNote = function (el) {
     var key = el.data('note');
     currentNotes[key] = el;
     el.addClass('selected');
+    $currentNotesDisplay.text(Object.keys(currentNotes));
   };
 
-  var removeNote = function(el) {
+  var removeNote = function (el) {
     var key = el.data('note');
     delete currentNotes[key];
     el.removeClass('selected');
+    $currentNotesDisplay.text(Object.keys(currentNotes));
   };
 
-  var displayNotes = function() {
-    for(var key in currentNotes) {
-      currentNotes[key].removeClass('selected');
+  var submitNotes = function () {
+    if (Object.keys(currentNotes).length && currentNotesDuration) { //if there are notes and a duration is specified
+      for (var key in currentNotes) {
+        currentNotes[key].removeClass('selected');
+      }
+      console.log(Object.keys(currentNotes));
+      $notesDurationDisplay.text("");
+      $currentNotesDisplay.text("");
     }
-    console.log(Object.keys(currentNotes));
   };
 
-  $(document).ready(function() {
+  var handleKeypress = function (event) {
+    var whichKey = event.which;
+
+    if (keyMap[whichKey]) {
+      currentNotesDuration = keyMap[whichKey];
+      $notesDurationDisplay.text(keyMap[whichKey]);
+      console.log($notesDurationDisplay.text());
+    } else if (whichKey === 13) { //enter
+      submitNotes();
+    }
+  };
+
+  $(document).ready(function () {
     // Add or Remove song when clicked
-    $('.piano').on('click', '.note', function() {
-      if(this.classList.contains('selected')) {
+    $('.piano').on('click', '.note', function () {
+      if (this.classList.contains('selected')) {
         removeNote($(this));
       } else {
         addNote($(this));
       }
     });
-    // Display notes
-    $('button').on('click', displayNotes);
+    // keypress handling
+    $(document).keypress(handleKeypress);
   });
 
 })();
